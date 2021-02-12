8d480400498a13dd1e1c91c4d6c8bdf109461787
Samiran Saha <samiran.s89@gmail.com>
2021-02-13 00:44:41 +0530

    trimmed down version of package.json and updated log level = debug

--- wdio.conf.js
+++ wdio.conf.js
@@ -122,7 +122,7 @@ exports.config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'error',
+    logLevel: 'debug',
     outputDir: path.join(__dirname, '/logs'),
     //
     // Set specific log levels per logger
